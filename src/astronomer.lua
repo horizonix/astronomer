a817b09b806e85248ae94f79b89bcca1d46a8811
horizonix <git@zack.pevcot.net>
2024-05-19 21:42:50 +0100

    Update astronomer.lua

--- src/astronomer.lua
+++ src/astronomer.lua
@@ -2,4 +2,5 @@
 --// Made by Horizonix Studios
 --/ Plugin Version: 1000/
 
+print('Successfully loaded');
 tweener:SetButtonDefaultTweens(UI.Background.Bar.VisualsButton);tweener:SetButtonDefaultTweens(UI.Background.Bar.LocalPlayerButton);tweener:SetButtonDefaultTweens(UI.Background.Bar.AboutButton);
