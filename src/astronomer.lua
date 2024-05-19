026f16281f8cc038bc366d59ae7b780fb8b53ce1
horizonix <git@zack.pevcot.net>
2024-05-19 21:37:53 +0100

    Update astronomer.lua

--- src/astronomer.lua
+++ src/astronomer.lua
@@ -1,3 +1,5 @@
 --/// Astronomer - Roblox Studio Plugin
 --// Made by Horizonix Studios
 --/ Plugin Version: 1000/
+
+tweener:SetButtonDefaultTweens(UI.Background.Bar.VisualsButton);tweener:SetButtonDefaultTweens(UI.Background.Bar.LocalPlayerButton);tweener:SetButtonDefaultTweens(UI.Background.Bar.AboutButton);
